c16f443653df6d1d4fc0ebafee855f7060118a30
Patryk Kotlarz <pkotlarz@suncode.pl>
2018-10-03 10:26:18 +0200

    Enable gzip

--- src/main/java/com/kotlarz/App.java
+++ src/main/java/com/kotlarz/App.java
@@ -12,7 +12,10 @@ public class App
         Service service = Service.ignite();
         service.port( 8888 );
 
-        service.before( ( request, response ) -> response.header( "Access-Control-Allow-Origin", "*" ) );
+        service.before( ( request, response ) -> {
+            response.header( "Access-Control-Allow-Origin", "*" );
+            response.header( "Content-Encoding", "gzip" );
+        } );
 
         service.get( "/", ( request, response ) -> {
             String command = request.queryParams( "command" );
