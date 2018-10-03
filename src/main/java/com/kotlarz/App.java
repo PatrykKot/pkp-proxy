2adfa60df72453376731bfd3839a4f5b020cd5b0
Patryk Kotlarz <pkotlarz@suncode.pl>
2018-10-03 10:21:56 +0200

    Enable cors

--- src/main/java/com/kotlarz/App.java
+++ src/main/java/com/kotlarz/App.java
@@ -12,6 +12,8 @@ public class App
         Service service = Service.ignite();
         service.port( 8888 );
 
+        service.before( ( request, response ) -> response.header( "Access-Control-Allow-Origin", "*" ) );
+
         service.get( "/", ( request, response ) -> {
             String command = request.queryParams( "command" );
             String pattern = request.queryParamOrDefault( "pattern", "" );
