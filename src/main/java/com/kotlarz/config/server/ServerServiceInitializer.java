f3a6e38b8bc625e6922bf01b509b12301f039b4c
Patryk Kotlarz <pkotlarz@suncode.pl>
2018-10-12 18:29:10 +0200

    HTTPS logs

--- src/main/java/com/kotlarz/config/server/ServerServiceInitializer.java
+++ src/main/java/com/kotlarz/config/server/ServerServiceInitializer.java
@@ -1,36 +1,33 @@
 package com.kotlarz.config.server;
 
 import com.kotlarz.AppArguments;
+import lombok.extern.slf4j.Slf4j;
 import spark.Service;
 
-public class ServerServiceInitializer
-{
-    public static Service start()
-    {
+@Slf4j
+public class ServerServiceInitializer {
+    public static Service start() {
         Service service = Service.ignite();
-        initHttps( service );
-        initBeforeFilter( service );
+        initHttps(service);
+        initBeforeFilter(service);
         return service;
     }
 
-    private static void initHttps( Service service )
-    {
-        if ( AppArguments.KEYSTORE_PATH != null && AppArguments.KEYSTORE_PASSWORD != null )
-        {
-            service.port( 8443 );
-            service.secure( AppArguments.KEYSTORE_PATH, AppArguments.KEYSTORE_PASSWORD, null, null );
-        }
-        else
-        {
-            service.port( 8080 );
+    private static void initHttps(Service service) {
+        if (AppArguments.KEYSTORE_PATH != null && AppArguments.KEYSTORE_PASSWORD != null) {
+            log.info("Enabling HTTPS");
+            service.port(8443);
+            service.secure(AppArguments.KEYSTORE_PATH, AppArguments.KEYSTORE_PASSWORD, null, null);
+        } else {
+            log.info("Enabling HTTP");
+            service.port(8080);
         }
     }
 
-    private static void initBeforeFilter( Service service )
-    {
-        service.before( ( request, response ) -> {
-            response.header( "Access-Control-Allow-Origin", "*" );
-            response.header( "Content-Encoding", "gzip" );
-        } );
+    private static void initBeforeFilter(Service service) {
+        service.before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Content-Encoding", "gzip");
+        });
     }
 }
