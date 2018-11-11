6aa7d1608d47c475a6308507749a24cad352837c
Patryk Kotlarz <pkotlarz@suncode.pl>
2018-11-11 22:53:54 +0100

    Search feature

--- src/main/java/com/kotlarz/transport/handler/TransportStopHandler.java
+++ src/main/java/com/kotlarz/transport/handler/TransportStopHandler.java
@@ -12,98 +12,131 @@ import java.util.concurrent.TimeUnit;
 
 @Bean
 public class TransportStopHandler
-        implements RequestHandler {
+                implements RequestHandler
+{
     private TransportService transportService;
 
     @Inject
-    public TransportStopHandler(TransportService transportService) {
+    public TransportStopHandler( TransportService transportService )
+    {
         this.transportService = transportService;
     }
 
     @Override
-    public void register(Service service) {
-        registerGetAllStops(service);
-        registerGetClosestStops(service);
-        registerGetClosestStopsNames(service);
-        registerGetCLosestStopsGrouped(service);
+    public void register( Service service )
+    {
+        registerGetAllStops( service );
+        registerGetClosestStops( service );
+        registerGetClosestStopsNames( service );
+        registerGetClosestStopsGrouped( service );
+        registerGetStopsByName( service );
     }
 
-    private void registerGetAllStops(Service service) {
-        service.get("stops", (request, response) -> {
-            HandlerUtils.asJson(response);
+    private void registerGetAllStops( Service service )
+    {
+        service.get( "stops", ( request, response ) -> {
+            HandlerUtils.asJson( response );
             HandlerUtils.cache( response, 1, TimeUnit.DAYS );
-            return HandlerUtils.toJson(transportService.getAllStops());
-        });
+            return HandlerUtils.toJson( transportService.getAllStops() );
+        } );
     }
 
-    private void registerGetClosestStops(Service service) {
-        service.get("stops/closest", (request, response) -> {
-            String longitude = request.queryParams("longitude");
-            String latitude = request.queryParams("latitude");
-            String limit = request.queryParams("limit");
+    private void registerGetClosestStops( Service service )
+    {
+        service.get( "stops/closest", ( request, response ) -> {
+            String longitude = request.queryParams( "longitude" );
+            String latitude = request.queryParams( "latitude" );
+            String limit = request.queryParams( "limit" );
 
-            Assert.hasLength(longitude, "Longitude cannot be empty");
-            Assert.hasLength(latitude, "Latitude cannot be empty");
-            Assert.hasLength(limit, "Limit cannot be empty");
+            Assert.hasLength( longitude, "Longitude cannot be empty" );
+            Assert.hasLength( latitude, "Latitude cannot be empty" );
+            Assert.hasLength( limit, "Limit cannot be empty" );
 
-            HandlerUtils.asJson(response);
-            HandlerUtils.cache( response, 1, TimeUnit.DAYS );
-            return HandlerUtils.toJson
-                    (
-                            transportService.getClosest(
-                                    Double.parseDouble(longitude),
-                                    Double.parseDouble(latitude),
-                                    Long.parseLong(limit)
-                            )
-                    );
-        });
-    }
-
-    private void registerGetClosestStopsNames(Service service) {
-        service.get("stops/closest/names", (request, response) -> {
-            String longitude = request.queryParams("longitude");
-            String latitude = request.queryParams("latitude");
-            String limit = request.queryParams("limit");
-
-            Assert.hasLength(longitude, "Longitude cannot be empty");
-            Assert.hasLength(latitude, "Latitude cannot be empty");
-            Assert.hasLength(limit, "Limit cannot be empty");
-
-            HandlerUtils.asJson(response);
+            HandlerUtils.asJson( response );
             HandlerUtils.cache( response, 1, TimeUnit.DAYS );
             return HandlerUtils.toJson
-                    (
-                            transportService.getClosestNames
-                                    (
-                                            Double.parseDouble(longitude),
-                                            Double.parseDouble(latitude),
-                                            Long.parseLong(limit)
-                                    )
-                    );
-        });
+                            (
+                                            transportService.getClosest(
+                                                            Double.parseDouble( longitude ),
+                                                            Double.parseDouble( latitude ),
+                                                            Long.parseLong( limit )
+                                            )
+                            );
+        } );
     }
 
-    private void registerGetCLosestStopsGrouped(Service service) {
-        service.get("stops/closest/grouped", (request, response) -> {
-            String longitude = request.queryParams("longitude");
-            String latitude = request.queryParams("latitude");
-            String limit = request.queryParams("limit");
+    private void registerGetClosestStopsNames( Service service )
+    {
+        service.get( "stops/closest/names", ( request, response ) -> {
+            String longitude = request.queryParams( "longitude" );
+            String latitude = request.queryParams( "latitude" );
+            String limit = request.queryParams( "limit" );
 
-            Assert.hasLength(longitude, "Longitude cannot be empty");
-            Assert.hasLength(latitude, "Latitude cannot be empty");
-            Assert.hasLength(limit, "Limit cannot be empty");
+            Assert.hasLength( longitude, "Longitude cannot be empty" );
+            Assert.hasLength( latitude, "Latitude cannot be empty" );
+            Assert.hasLength( limit, "Limit cannot be empty" );
 
-            HandlerUtils.asJson(response);
+            HandlerUtils.asJson( response );
             HandlerUtils.cache( response, 1, TimeUnit.DAYS );
             return HandlerUtils.toJson
-                    (
-                            transportService.getClosestGrouped
-                                    (
-                                            Double.parseDouble(longitude),
-                                            Double.parseDouble(latitude),
-                                            Long.parseLong(limit)
-                                    )
-                    );
-        });
+                            (
+                                            transportService.getClosestNames
+                                                            (
+                                                                            Double.parseDouble( longitude ),
+                                                                            Double.parseDouble( latitude ),
+                                                                            Long.parseLong( limit )
+                                                            )
+                            );
+        } );
+    }
+
+    private void registerGetClosestStopsGrouped( Service service )
+    {
+        service.get( "stops/closest/grouped", ( request, response ) -> {
+            String longitude = request.queryParams( "longitude" );
+            String latitude = request.queryParams( "latitude" );
+            String limit = request.queryParams( "limit" );
+
+            Assert.hasLength( longitude, "Longitude cannot be empty" );
+            Assert.hasLength( latitude, "Latitude cannot be empty" );
+            Assert.hasLength( limit, "Limit cannot be empty" );
+
+            HandlerUtils.asJson( response );
+            HandlerUtils.cache( response, 1, TimeUnit.DAYS );
+            return HandlerUtils.toJson
+                            (
+                                            transportService.getClosestGrouped
+                                                            (
+                                                                            Double.parseDouble( longitude ),
+                                                                            Double.parseDouble( latitude ),
+                                                                            Long.parseLong( limit )
+                                                            )
+                            );
+        } );
+    }
+
+    private void registerGetStopsByName( Service service )
+    {
+        service.get( "stops/search", ( request, response ) -> {
+            String name = request.queryParamOrDefault( "name", "" );
+            String start = request.queryParams( "start" );
+            String limit = request.queryParams( "limit" );
+
+            Assert.hasLength( start, "Start cannot be empty" );
+            Assert.hasLength( limit, "Limit cannot be empty" );
+
+            HandlerUtils.asJson( response );
+            HandlerUtils.cache( response, 1, TimeUnit.DAYS );
+
+            return HandlerUtils.toJson
+                            (
+                                            transportService.getByName
+                                                            (
+                                                                            name,
+                                                                            Integer.parseInt( start ),
+                                                                            Integer.parseInt( limit )
+                                                            )
+                            );
+        } );
     }
 }
